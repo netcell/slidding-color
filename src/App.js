aaec60aa28dde9b0af8222c2b8fbe02aaf859a89
Nguyễn Tuấn Anh <anhnt.fami@gmail.com>
2020-04-19 05:04:01 +0700

    update

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ import { Swipeable } from "react-swipeable";
 console.log(bg);
 
 export default function App() {
-  const [size, setSize] = useState(6);
-  const [numBlock, setNumBlock] = useState(6);
+  const [size, setSize] = useState(5);
+  const [numBlock, setNumBlock] = useState(3);
+  const [space, setSpace] = useState(1);
+  const [showConfig, setShowConfig] = useState(true);
+  const [difficulty, setDifficulty] = useState(0);
 
   const [gameState, setGameState] = useState([]);
 
@@ -47,7 +50,7 @@ export default function App() {
     const blocks = _.shuffle(positions).slice(0, numBlock);
 
     let state = _.chain(positions)
-      .slice(0, -1)
+      .slice(0, -space)
       .map((position, index) => {
         const colorIndex = (index / size) >> 0;
         if (blocks.indexOf(position) >= 0)
@@ -67,6 +70,8 @@ export default function App() {
       .keyBy(({ row, col }) => `${row},${col}`)
       .value();
 
+    const originalState = state;
+
     for (let i = 0; i < 1000; i++) {
       state = actionCalc(
         _.sample([0, 1])
@@ -82,10 +87,22 @@ export default function App() {
       );
     }
 
+    const difficulty = _.reduce(
+      state,
+      (difficulty, { col, color }) => {
+        const colorIndex = colors.indexOf(color);
+        if (colorIndex < 0) return difficulty;
+        else return difficulty + Math.pow(col - colorIndex, 2);
+      },
+      0
+    );
+
+    setDifficulty(difficulty);
+
     setGameState(state);
   };
 
-  useEffect(init, [size, numBlock]);
+  useEffect(init, [size, numBlock, space]);
 
   const changeCord = (position, direction) => {
     const newPosition = position + direction;
@@ -147,77 +164,142 @@ export default function App() {
         }}
         tabIndex="0"
       >
-        <p
+        <div
           style={{
-            marginBottom: "20px",
-            width: 300,
-            margin: "30px auto 40px"
+            height: showConfig ? 200 : 0,
+            overflow: "hidden",
+            transition: "ease all 0.2s"
           }}
         >
-          <Slider
-            dots
-            step={1}
-            value={size}
-            min={2}
-            max={12}
-            onChange={setSize}
-          />
-        </p>
-        <p
-          style={{
-            marginBottom: "20px",
-            width: 300,
-            margin: "30px auto 40px"
-          }}
-        >
-          <Slider
-            dots
-            step={1}
-            value={numBlock}
-            min={0}
-            max={12}
-            onChange={setNumBlock}
-          />
-        </p>
+          <p
+            style={{
+              width: 300,
+              margin: "0px auto 40px"
+            }}
+          >
+            <label style={{ color: "white" }}>Size {size}</label>
+            <Slider
+              dots
+              step={1}
+              value={size}
+              min={3}
+              max={6}
+              onChange={setSize}
+            />
+          </p>
+          <p
+            style={{
+              width: 300,
+              margin: "30px auto 40px"
+            }}
+          >
+            <label style={{ color: "white" }}>{numBlock} Obstacles</label>
+            <Slider
+              dots
+              step={1}
+              value={numBlock}
+              min={0}
+              max={6}
+              onChange={setNumBlock}
+            />
+          </p>
+          <p
+            style={{
+              marginBottom: "20px",
+              width: 300,
+              margin: "30px auto 40px"
+            }}
+          >
+            <label style={{ color: "white" }}>{space} Blanks</label>
+            <Slider
+              dots
+              step={1}
+              value={space}
+              min={1}
+              max={4}
+              onChange={setSpace}
+            />
+          </p>
+        </div>
+
         <p>
           <span
-            onClick={init}
+            onClick={() => setShowConfig(!showConfig)}
             style={{
-              padding: 5,
+              padding: 10,
               color: "black",
               fontSize: "20px",
               textTransform: "uppercase",
               fontWeight: "bold",
-              background: "white",
+              background: "#FFD200",
               cursor: "pointer",
-              userSelect: "none"
+              userSelect: "none",
+              borderRadius: 10,
+              boxShadow: "0 5px 0px 1px #CD5900"
+            }}
+          >
+            {showConfig ? "Hide" : "Show"} Config
+          </span>{" "}
+          &emsp;
+          <span
+            onClick={init}
+            style={{
+              padding: 10,
+              color: "black",
+              fontSize: "20px",
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              background: "#FFD200",
+              cursor: "pointer",
+              userSelect: "none",
+              borderRadius: 10,
+              boxShadow: "0 5px 0px 1px #CD5900"
             }}
           >
             Reset
           </span>
         </p>
+        {/* <p>
+          <span
+            style={{
+              padding: 5,
+              color: "white",
+              fontSize: "20px",
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              cursor: "pointer",
+              userSelect: "none"
+            }}
+          >
+            Difficulty {difficulty}
+          </span>
+        </p> */}
         <div
           style={{
-            width: size * 60 - 30,
-            height: size * 60 - 30,
+            width: size * 60 + 20,
+            height: size * 60 + 20,
             position: "relative",
             margin: "auto",
-            marginTop: 50
+            marginTop: 50,
+            border: "2px solid #BBBABB",
+            background: "rgba(255, 255, 255, 0.1)",
+            borderRadius: 30
           }}
         >
           {_.map(gameState, ({ row, col, color, key, locked }) => (
             <div
               key={key}
               style={{
-                borderRadius: locked ? 10 : 30,
+                borderRadius: locked ? 15 : 45,
                 background: color,
-                width: 30,
-                height: 30,
+                width: 45,
+                height: 45,
                 position: "absolute",
-                left: col * 60,
-                top: row * 60,
-                transition: "all ease 0.1s",
-                border: "2px white solid"
+                left: col * 60 + 15,
+                top: row * 60 + 15,
+                transition: "all ease 0.2s",
+                border: `2px white solid`,
+                boxShadow: "0 0 10px 10px rgba(0, 0, 0, 0.5)"
               }}
             />
           ))}
